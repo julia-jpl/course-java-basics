39cc251b39d09ef29533aa134b4a6fe00f3e3647
Julia Portnova <portnova.julia@gmail.com>
2020-10-07 09:08:46 +0300

    Feat: done task10 but not tested

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task10.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task10.java
@@ -28,5 +28,26 @@ public class Task10 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        if (firstFuelReserves == secondFuelReserves && firstFuelReserves == thirdFuelReserves) {
+            System.out.println("1 2 3");
+        } else if (firstFuelReserves < secondFuelReserves) {
+                if (firstFuelReserves < thirdFuelReserves) {
+                    System.out.println("1");
+                } else if (firstFuelReserves == thirdFuelReserves) {
+                    System.out.println("1 3");
+                } else {
+                    System.out.println("3");
+                }
+            } else if (secondFuelReserves < firstFuelReserves) {
+                if (secondFuelReserves < thirdFuelReserves) {
+                    System.out.println("2");
+                } else if (secondFuelReserves == thirdFuelReserves) {
+                    System.out.println("2 3");
+                } else {
+                    System.out.println("3");
+                }
+            } else {
+            System.out.println("1 2");
+        }
     }
-}
+    }
