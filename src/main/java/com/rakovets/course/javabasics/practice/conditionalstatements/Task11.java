dbd5897f6d258646248c9ec20e308fe3da9ba9f2
Julia Portnova <portnova.julia@gmail.com>
2020-10-07 11:24:20 +0300

    Feat: done task11 but not tested

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
@@ -29,5 +29,16 @@ public class Task11 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        long unicodLetter = letter;
+        if (unicodLetter >= 48 && unicodLetter <= 57) {
+            System.out.println("digit");
+        } else if (unicodLetter >= 1024 && unicodLetter <= 1274) {
+            System.out.println("cyrillic");
+        } else if ((unicodLetter >= 65 && unicodLetter <= 90) || (unicodLetter >= 97 && unicodLetter <= 122)) {
+            System.out.println("latin");
+        } else if (unicodLetter < 47 || (unicodLetter >= 58 && unicodLetter <= 64) || (unicodLetter >= 91 && unicodLetter <= 96) || (unicodLetter >= 123 && unicodLetter <= 191)) {
+            System.out.println("undefined");
+        }
+
     }
 }
