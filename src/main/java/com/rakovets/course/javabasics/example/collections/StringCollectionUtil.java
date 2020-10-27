1a6122dd944dd37a17522194555671f08108f97c
Julia Portnova <portnova.julia@gmail.com>
2020-10-27 13:16:36 +0300

    feat: only started tasks

---
+++ src/main/java/com/rakovets/course/javabasics/example/collections/StringCollectionUtil.java
@@ -0,0 +1,11 @@
+package com.rakovets.course.javabasics.example.collections;
+
+import java.util.Collection;
+
+public class StringCollectionUtil {
+
+    public String resetWordsByLength(Collection<String> collection, int size) {
+        if
+
+    }
+}
